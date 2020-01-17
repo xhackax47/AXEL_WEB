3c570bf7536c38f0100380e52b7a38af07049032
Samy Chaabi <xhackax47@gmail.com>
2020-01-17 23:34:21 +0100

    Mode Production Heroku : Modification vues AccountSettings

--- AXEL_WEB/settings.py
+++ AXEL_WEB/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = ')k7-35hzr=44j&_nls3u%*ne1xz@==1gt(1k9-6%ra!y6pk21l'
 DEBUG = False
 if DEBUG == True:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
-ALLOWED_HOSTS = ['axel-ihm.herokuapp.com']
+else:
+    ALLOWED_HOSTS = ['axel-ihm.herokuapp.com']
 
 # Applications installés
 
