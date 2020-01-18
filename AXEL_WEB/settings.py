69a3d35ce118de6129c2204f7002bf06f51f8da2
Samy Chaabi <xhackax47@gmail.com>
2020-01-18 12:14:32 +0100

    DEBUG = FALSE

--- AXEL_WEB/settings.py
+++ AXEL_WEB/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')k7-35hzr=44j&_nls3u%*ne1xz@==1gt(1k9-6%ra!y6pk21l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 if DEBUG:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 else:
