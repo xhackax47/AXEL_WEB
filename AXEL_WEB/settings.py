2543b8d08bbefd921d207a639aa81f74418e10c3
Samy Chaabi <xhackax47@gmail.com>
2020-01-19 19:49:53 +0100

    Fix issue : SMTPAuthenticationError

--- AXEL_WEB/settings.py
+++ AXEL_WEB/settings.py
@@ -96,7 +96,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'xhackax47@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', encrypt256('N@dyalilou71300'))
+#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', encrypt256('N@dyalilou71300'))
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'jtgrdelljxadtrqg')
 EMAIL_PORT = 587
 
 TEMPLATES = [
