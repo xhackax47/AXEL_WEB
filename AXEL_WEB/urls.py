7c1bd5cb4e156658e185495f737d78334364ab24
Samy <xhackax47@gmail.com>
2020-01-22 15:40:04 +0100

    Ajout page construction

--- AXEL_WEB/urls.py
+++ AXEL_WEB/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from WebAXEL import views
+from WebAXEL.views import ConstructionView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('adminAXEL/', include('AdminAXEL.urls')),
+    #path('adminAXEL/', include('AdminAXEL.urls')),
+    path('adminAXEL/', ConstructionView.as_view(), name='construction'),
     path('api/v1/', include('api.urls')),
     path('', include('WebAXEL.urls')),
 ]
